3d6a119c4683588550985b6beb298f9b4e38c5ba
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-05-11 17:02:31 +0600

    Fix webpack undici error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,24 @@ const nextConfig = {
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      };
+    }
+    config.externals = config.externals || [];
+    if (!isServer) {
+      config.externals.push({
+        'undici': 'undici',
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
