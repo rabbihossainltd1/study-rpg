aa6b123a7478732a10889311c5865e13c153a753
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-05-14 02:05:29 +0600

    Add files via upload

--- frontend/android/app/src/main/java/com/rabbi/studyrpg/app/MainActivity.kt
+++ frontend/android/app/src/main/java/com/rabbi/studyrpg/app/MainActivity.kt
@@ -1,39 +1,16 @@
 package com.rabbi.studyrpg.app
 
 import android.content.Intent
-import android.util.Log
+import android.os.Bundle
 import com.getcapacitor.BridgeActivity
-import ee.forgr.capacitor.social.login.GoogleProvider
-import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin
-import ee.forgr.capacitor.social.login.SocialLoginPlugin
 
-class MainActivity : BridgeActivity(), ModifiedMainActivityForSocialLoginPlugin {
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+class MainActivity : BridgeActivity() {
 
-        if (
-            requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN &&
-            requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX
-        ) {
-            val pluginHandle = bridge?.getPlugin("SocialLogin")
-
-            if (pluginHandle == null) {
-                Log.i("GoogleActivityResult", "SocialLogin plugin handle is null")
-                return
-            }
-
-            val plugin = pluginHandle.instance
-
-            if (plugin !is SocialLoginPlugin) {
-                Log.i("GoogleActivityResult", "SocialLogin plugin instance is not SocialLoginPlugin")
-                return
-            }
-
-            plugin.handleGoogleLoginIntent(requestCode, data)
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    override fun IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
-        // Required by @capgo/capacitor-social-login.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
