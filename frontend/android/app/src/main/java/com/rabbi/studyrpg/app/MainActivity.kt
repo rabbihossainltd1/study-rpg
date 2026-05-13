4dc3f77a81532cdfe1d63db95c37bfbf629df311
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-05-14 04:57:27 +0600

    Add files via upload

--- frontend/android/app/src/main/java/com/rabbi/studyrpg/app/MainActivity.kt
+++ frontend/android/app/src/main/java/com/rabbi/studyrpg/app/MainActivity.kt
@@ -3,12 +3,10 @@ package com.rabbi.studyrpg.app
 import android.content.Intent
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth
 
 class MainActivity : BridgeActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        registerPlugin(GoogleAuth::class.java)
         super.onCreate(savedInstanceState)
     }
 
