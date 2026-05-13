a611cc2d15a9d04580f5ef51a0009492d7f71fef
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-05-14 00:10:13 +0600

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,38 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        stream: false,
-        url: false,
-        zlib: false,
-        http: false,
-        https: false,
-        assert: false,
-        os: false,
-        path: false,
-      };
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        undici: false,
-      };
-    }
-    return config;
-  },
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
